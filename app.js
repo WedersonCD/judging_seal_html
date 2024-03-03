beed0b821c7a7665dfb030ccc539f5e153fc456b
wederson <wedersoncabral@gmail.com>
2024-03-03 17:36:28 -0300

    base line index file working

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/src/views');
+app.use(express.static('public'));
+
+//ROUTES
+var router = require(__dirname+'/src/routes/index');
+
+app.use('/', router);
+
+
+
+const port = process.env.SERVICE_PORT || 9953
+app.listen(port, () => console.log(`HTML server listening on port ${port}`));
