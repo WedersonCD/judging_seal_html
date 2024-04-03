ddb748733a65a0e25954724236d27a466e7dd1fe
Wederson Cabral Domingues <28493584+WedersonCD@users.noreply.github.com>
2024-04-03 17:59:53 -0300

    Update utils.js

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,13 +1,10 @@
 const utils={}
 
 utils.dateTimeStringToDate = (dateInString)=>{
-console.log('Before: ',dateInString)
     
     //chaging the timeZone to UTC-3
     let date = new Date(dateInString);
 
-    console.log('After: ',date)
-    console.log('After 2: ',date.toLocaleDateString('pt-BR',{timeZone: 'America/Sao_Paulo'}))
     return date.toLocaleDateString('pt-BR',{timeZone: 'America/Sao_Paulo'})
 
 }
